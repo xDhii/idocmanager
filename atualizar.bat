62af81a4504df978442386f552031d62fdcd30ee
Adriano Valumin <adriano.valumin@outlook.com>
2019-07-02 15:42:10 -0300

    mensagens antes de atualizar

--- atualizar.bat
+++ atualizar.bat
@@ -1,3 +1,16 @@
+@echo off
+@break off
+cls
+echo Atualizador
+
+echo Este processo ira atualizar o projeto de automacao do TF.
+echo Este processo leva ate 2 minutos
+echo .
+echo .
+echo .
+sleep 5
+
+
 git init
 git pull https://github.com/xDhii/idocmanager.git
 CD .git
