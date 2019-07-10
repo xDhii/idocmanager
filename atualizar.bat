cafdecc7838169b35bf2dba9b6f48d342d488ec3
Adriano Valumin <adriano.valumin@outlook.com>
2019-07-10 20:01:26 -0300

    Ajustes no atualizador

--- atualizar.bat
+++ atualizar.bat
@@ -4,16 +4,11 @@ cls
 echo Atualizador
 
 echo Este processo ira atualizar o projeto de automacao do TF.
-echo Este processo leva ate 2 minutos
+echo Este processo pode levar ate 2 minutos
 echo .
 echo .
 echo .
-sleep 5
 
 
 git init
 git pull https://github.com/xDhii/idocmanager.git
-CD .git
-del /F/Q/S *.* > NUL
-cd ..
-RMDIR /Q/S .git
