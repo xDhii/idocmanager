bfc67362b29d6fb5d350c7940909e798169c25b4
Adriano Valumin <adriano.valumin@outlook.com>
2019-07-12 14:23:09 -0300

    Implemented log clean function

--- resetfolio.py
+++ resetfolio.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, time
 zerar = 0
 ## ARG ##
 f = open('./select/ARG/folio.txt', 'w')
@@ -20,3 +20,24 @@ f.close()
 f = open('./select/URY/folio.txt', 'w')
 print(zerar, file=f)
 f.close()
+
+print('Okay, all the folios was reseted.')
+print('Do you want to clean all the log files?')
+cleanlog = input('Y to clean or N to leave: ')
+cleanlog = cleanlog.lower()
+if cleanlog in ('Y', 'y'):
+    print('Roger that!')
+    f = open('bin/country.log', 'w')
+    f.write('\n')
+    f.close()
+    f = open('bin/document.log', 'w')
+    f.write('\n')
+    f.close()
+    f = open('bin/folio.log', 'w')
+    f.write('\n')
+    f.close()
+    print('Everything is clear!')
+else:
+    print('I do not understood your answer...')
+    print('I will not touch your logs.')
+time.sleep(5)
