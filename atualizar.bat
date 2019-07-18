de81808ebcb1f599147c10557630c7c50b561650
Adriano Valumin <adriano.valumin@outlook.com>
2019-07-18 16:15:42 -0300

    updater

--- atualizar.bat
+++ atualizar.bat
@@ -9,19 +9,30 @@ echo .
 echo .
 
 
-rmdir bin, config, country, process, select, idocmanager /Q/S > NUL
-rm __init__.py, idocmanager.py, Iniciar.bat, README.ba /Q/F/S > NUL
+rmdir bin, config, country, process, select, idocmanager, .git, git /Q/S > NUL
+del __init__.py /Q/F/S > NULdel idocmanager.py /Q/F/S > NUL
+del Iniciar.bat /Q/F/S > NUL
+del README.md /Q/F/S > NUL
 git clone https://github.com/xDhii/idocmanager.git
 
 cd idocmanager
+attrib -s -h .git
+echo
+ren .git git
+move git ..
 move bin ..
 move config ..
 move country ..
 move process ..
 move select ..
-move *.* ..
+move idocmanager.py ..
+move Iniciar.bat ..
+move README.md ..
+move __init__.py
 cd ..
-
+ren git .git
+attrib +h .git
+rmdir idocmanager /Q/S > NUL
 
 
 echo Okay, finished ;)
