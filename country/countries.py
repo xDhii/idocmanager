1a79f99e4f57ca19a5a73522d909b10f0feb9e2d
Adriano Valumin <adriano.valumin@outlook.com>
2019-07-30 18:33:17 -0300

    Just a test

--- country/countries.py
+++ country/countries.py
@@ -87,6 +87,119 @@ def Brasil():
     f = open('./bin/document.log', 'w')
     f.write(document)
     f.close()
+def Chile():
+    ## Mensagem inicial ##
+    messages.limpar_tela()
+    messages.mensageminicial()
+    ## Voltar para a pasta de documentos do Chile ##
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/country/documents")
+    import documents
+    ## Opçoes de documentos disponiveis ##
+    print('Available documents: ')
+    print()
+    print('\033[1;30;47m A \033[0m - \033[4mCHL - DTE                            \033[0m')
+    print('\033[1;30;47m B \033[0m - \033[4mCHL - DTE DSCCItem 1000+             \033[0m')
+    print('\033[1;30;47m C \033[0m - \033[4mCHL - DTE Cesion             \033[0m')
+
+    print()
+    print('Got it. What kind of document do you want to create?')
+    opt = input('Enter the letter corresponding to the document type: ')
+    opt = opt.lower()
+    while opt not in ('a', 'b', 'c'):
+        print('\033[91m Hmm... I did not understand which document you want to generate. \033[0m')
+        opt = input("Let's try again. Enter the corresponding letter: ")
+        opt = opt.lower()
+
+    if opt == "a":
+        document = "Chile DTE"
+        documents.CHL_dte()
+    if opt == "b":
+        document = "Chile DTE DSCCItem 1000+"
+        documents.CHL_dtedescitem1000()
+    if opt == "c":
+        document = "Chile DTE Cesion"
+        documents.CHL_cesion()
+    ## Criar log da Seleção ##
+    f = open('./bin/country.log', 'w')
+    f.write('Chile')
+    f.close()
+    f = open('./bin/document.log', 'w')
+    f.write(document)
+    f.close()
+def Ecuador():
+    ## Mensagem inicial ##
+    messages.limpar_tela()
+    messages.mensageminicial()
+    ## Voltar para a pasta de documentos do Ecuador ##
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/country/documents")
+    import documents
+    ## Opçoes de documentos disponiveis ##
+    print('Available documents: ')
+    print()
+    print('\033[1;30;47m A \033[0m - \033[4mECU - Factura 01              \033[0m')
+    print('\033[1;30;47m B \033[0m - \033[4mECU - Remision 06             \033[0m')
+
+    print()
+    print('Got it. What kind of document do you want to create?')
+    opt = input('Enter the letter corresponding to the document type: ')
+    opt = opt.lower()
+    while opt not in ('a', 'b', 'c'):
+        print('\033[91m Hmm... I did not understand which document you want to generate. \033[0m')
+        opt = input("Let's try again. Enter the corresponding letter: ")
+        opt = opt.lower()
+
+    if opt == "a":
+        document = "ECU Factura 01"
+        documents.ECU_factura01()
+    if opt == "b":
+        document = "ECU Remision 06"
+        documents.ECU_remision06()
+    ## Criar log da Seleção ##
+    f = open('./bin/country.log', 'w')
+    f.write('Ecuador')
+    f.close()
+    f = open('./bin/document.log', 'w')
+    f.write(document)
+    f.close()
+def CostaRica():
+    ## Mensagem inicial ##
+    messages.limpar_tela()
+    messages.mensageminicial()
+    ## Voltar para a pasta de documentos do Chile ##
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/country/documents")
+    import documents
+    ## Opçoes de documentos disponiveis ##
+    print('Available documents: ')
+    print()
+    print('\033[1;30;47m A \033[0m - \033[4mCHL - DTE                            \033[0m')
+    print('\033[1;30;47m B \033[0m - \033[4mCHL - DTE DSCCItem 1000+             \033[0m')
+    print('\033[1;30;47m C \033[0m - \033[4mCHL - DTE Cesion             \033[0m')
+
+    print()
+    print('Got it. What kind of document do you want to create?')
+    opt = input('Enter the letter corresponding to the document type: ')
+    opt = opt.lower()
+    while opt not in ('a', 'b', 'c'):
+        print('\033[91m Hmm... I did not understand which document you want to generate. \033[0m')
+        opt = input("Let's try again. Enter the corresponding letter: ")
+        opt = opt.lower()
+
+    if opt == "a":
+        document = "Chile DTE"
+        documents.CHL_dte()
+    if opt == "b":
+        document = "Chile DTE DSCCItem 1000+"
+        documents.CHL_dtedescitem1000()
+    if opt == "c":
+        document = "Chile DTE Cesion"
+        documents.CHL_cesion()
+    ## Criar log da Seleção ##
+    f = open('./bin/country.log', 'w')
+    f.write('Chile')
+    f.close()
+    f = open('./bin/document.log', 'w')
+    f.write(document)
+    f.close()
 def Mexico():
     ## Mensagem inicial ##
     messages.limpar_tela()
@@ -153,103 +266,6 @@ def Mexico():
         documents.MEX_cfdi33_pagos()
     f = open('./bin/country.log', 'w')
     f.write('México')
-    f.close()
-    f = open('./bin/document.log', 'w')
-    f.write(document)
-    f.close()
-def Uruguay():
-    ## Mensagem inicial ##
-    messages.limpar_tela()
-    messages.mensageminicial()
-    ## Voltar para a pasta de documentos do Brasil ##
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/country/documents")
-    import documents
-    ## Opçoes de documentos disponiveis ##
-    print('Available documents: ')
-    print()
-    print('\033[1;30;47m A \033[0m - \033[4mURY - 101 Ticket                            \033[0m')
-    print('\033[1;30;47m B \033[0m - \033[4mURY - 111 Factura                           \033[0m')
-    print('\033[1;30;47m C \033[0m - \033[4mURY - 121 Factura Exportación               \033[0m')
-    print('\033[1;30;47m D \033[0m - \033[4mURY - 122 Factura Nota Crédito Exportación  \033[0m')
-    print('\033[1;30;47m E \033[0m - \033[4mURY - 123 Factura Nota Débito Exportación   \033[0m')
-    print('\033[1;30;47m F \033[0m - \033[4mURY - 124 eRemito Exportación               \033[0m')
-    print('\033[1;30;47m G \033[0m - \033[4mURY - 211 Factura (for failure)              \033[0m')
-    print('\033[1;30;47m H \033[0m - \033[4mURY - 211 Factura                            \033[0m')
-
-    print()
-    print('Got it. What kind of document do you want to create?')
-    opt = input('Enter the letter corresponding to the document type: ')
-    opt = opt.lower()
-    while opt not in ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'):
-        print('\033[91m Hmm... I did not understand which document you want to generate. \033[0m')
-        opt = input("Let's try again. Enter the corresponding letter: ")
-        opt = opt.lower()
-
-    if opt == "a":
-        document = "Uruguay Ticket (101)"
-        documents.URY_101ticket()
-    if opt == "b":
-        document = "Uruguay Factura (111)"
-        documents.URY_111factura()
-    if opt == "c":
-        document = "Uruguay Factura Exportacion (121)"
-        documents.URY_121facturaexportacion()
-    if opt == "d":
-        document = "Uruguay Factura Nota Credito(122)"
-        documents.URY_122facturanotacredito()
-    if opt == "e":
-        document = "Uruguay Factura Nota Debito (123)"
-        documents.URY_123facturanotadebito()
-    if opt == "f":
-        document = "Uruguay Eremito Exportacion (124)"
-        documents.URY_124eremitoexportacion()
-    if opt == "g":
-        document = "Uruguay Factura (for Failure)"
-        documents.URY_211errorce()
-    if opt == "h":
-        document = "Uruguay Factura (211)"
-        documents.URY_211factura()
-    f = open('./bin/country.log', 'w')
-    f.write('Uruguay')
-    f.close()
-    f = open('./bin/document.log', 'w')
-    f.write(document)
-    f.close()
-def Chile():
-    ## Mensagem inicial ##
-    messages.limpar_tela()
-    messages.mensageminicial()
-    ## Voltar para a pasta de documentos do Chile ##
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/country/documents")
-    import documents
-    ## Opçoes de documentos disponiveis ##
-    print('Available documents: ')
-    print()
-    print('\033[1;30;47m A \033[0m - \033[4mCHL - DTE                            \033[0m')
-    print('\033[1;30;47m B \033[0m - \033[4mCHL - DTE DSCCItem 1000+             \033[0m')
-    print('\033[1;30;47m C \033[0m - \033[4mCHL - DTE Cesion             \033[0m')
-
-    print()
-    print('Got it. What kind of document do you want to create?')
-    opt = input('Enter the letter corresponding to the document type: ')
-    opt = opt.lower()
-    while opt not in ('a', 'b', 'c'):
-        print('\033[91m Hmm... I did not understand which document you want to generate. \033[0m')
-        opt = input("Let's try again. Enter the corresponding letter: ")
-        opt = opt.lower()
-
-    if opt == "a":
-        document = "Chile DTE"
-        documents.CHL_dte()
-    if opt == "b":
-        document = "Chile DTE DSCCItem 1000+"
-        documents.CHL_dtedescitem1000()
-    if opt == "c":
-        document = "Chile DTE Cesion"
-        documents.CHL_cesion()
-    ## Criar log da Seleção ##
-    f = open('./bin/country.log', 'w')
-    f.write('Chile')
     f.close()
     f = open('./bin/document.log', 'w')
     f.write(document)
@@ -321,37 +337,60 @@ def Peru():
     f = open('./bin/document.log', 'w')
     f.write(document)
     f.close()
-def Ecuador():
+def Uruguay():
     ## Mensagem inicial ##
     messages.limpar_tela()
     messages.mensageminicial()
-    ## Voltar para a pasta de documentos do Ecuador ##
+    ## Voltar para a pasta de documentos do Brasil ##
     sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/country/documents")
     import documents
     ## Opçoes de documentos disponiveis ##
     print('Available documents: ')
     print()
-    print('\033[1;30;47m A \033[0m - \033[4mECU - Factura 01              \033[0m')
-    print('\033[1;30;47m B \033[0m - \033[4mECU - Remision 06             \033[0m')
+    print('\033[1;30;47m A \033[0m - \033[4mURY - 101 Ticket                            \033[0m')
+    print('\033[1;30;47m B \033[0m - \033[4mURY - 111 Factura                           \033[0m')
+    print('\033[1;30;47m C \033[0m - \033[4mURY - 121 Factura Exportación               \033[0m')
+    print('\033[1;30;47m D \033[0m - \033[4mURY - 122 Factura Nota Crédito Exportación  \033[0m')
+    print('\033[1;30;47m E \033[0m - \033[4mURY - 123 Factura Nota Débito Exportación   \033[0m')
+    print('\033[1;30;47m F \033[0m - \033[4mURY - 124 eRemito Exportación               \033[0m')
+    print('\033[1;30;47m G \033[0m - \033[4mURY - 211 Factura (for failure)              \033[0m')
+    print('\033[1;30;47m H \033[0m - \033[4mURY - 211 Factura                            \033[0m')
 
     print()
     print('Got it. What kind of document do you want to create?')
     opt = input('Enter the letter corresponding to the document type: ')
     opt = opt.lower()
-    while opt not in ('a', 'b', 'c'):
+    while opt not in ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'):
         print('\033[91m Hmm... I did not understand which document you want to generate. \033[0m')
         opt = input("Let's try again. Enter the corresponding letter: ")
         opt = opt.lower()
 
     if opt == "a":
-        document = "ECU Factura 01"
-        documents.ECU_factura01()
+        document = "Uruguay Ticket (101)"
+        documents.URY_101ticket()
     if opt == "b":
-        document = "ECU Remision 06"
-        documents.ECU_remision06()
-    ## Criar log da Seleção ##
+        document = "Uruguay Factura (111)"
+        documents.URY_111factura()
+    if opt == "c":
+        document = "Uruguay Factura Exportacion (121)"
+        documents.URY_121facturaexportacion()
+    if opt == "d":
+        document = "Uruguay Factura Nota Credito(122)"
+        documents.URY_122facturanotacredito()
+    if opt == "e":
+        document = "Uruguay Factura Nota Debito (123)"
+        documents.URY_123facturanotadebito()
+    if opt == "f":
+        document = "Uruguay Eremito Exportacion (124)"
+        documents.URY_124eremitoexportacion()
+    if opt == "g":
+        document = "Uruguay Factura (for Failure)"
+        documents.URY_211errorce()
+    if opt == "h":
+        document = "Uruguay Factura (211)"
+        documents.URY_211factura()
     f = open('./bin/country.log', 'w')
-    f.write('Ecuador')
+    f.write('Uruguay')
     f.close()
     f = open('./bin/document.log', 'w')
     f.write(document)
