c57e9d8a10f4f72d9272b87eb284676cbc73f71f
Adriano Valumin <adriano.valumin@outlook.com>
2019-12-31 15:35:12 -0300

    version adjustment

--- config/messages.py
+++ config/messages.py
@@ -7,7 +7,7 @@ def limpar_tela():
 def mensageminicial():
     print('\033[91m IMPORTANT: All companies must be configured as SAPKFQ!        \033[0m')
     print()
-    print("\033[1;30;47m            IDOC CREATOR v0.7.0807 (beta)                 \033[0m")
+    print("\033[1;30;47m            IDOC CREATOR v1.0 (beta)                 \033[0m")
     print()
     print('\33[1;37;40m Is any company or document missing? Any BUG? Contact us:         \033[0m')
     print('\33[1;37;40m adriano.valumin@sovos.com / tayna.blasques@sovos.com    \033[0m')
